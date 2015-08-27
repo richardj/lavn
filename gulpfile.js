931aa556ab78a70db5baa8fbf3076270235d7da5
Richard Japenga <treurmars@gmail.com>
2015-08-27 09:22:01 +0200

    updated paths in gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var gulp          = require('gulp');
+var sass          = require('gulp-sass');
+var concat        = require('gulp-concat');
+var rename        = require('gulp-rename');
+var sourcemaps    = require('gulp-sourcemaps');
+var autoprefixer  = require('gulp-autoprefixer');
+
+
+var sassSrc     = './src/sass';
+var cssDest     = './public/css';
+var jsDest      = './public/scripts';
+
+gulp.task('sass', function() {
+  gulp.src('./src/sass/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(autoprefixer())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(sass({
+    //indentedSyntax: true,
+    includePaths: './src/sass/*',
+    sourceComments: 'normal'
+    // outputStyle: 'compressed'
+  }))
+  .pipe(sourcemaps.write('./maps'))
+  .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(sassSrc + '/**/*.scss', ['sass']);
+
+});
+
+gulp.task('default', ['watch']);
